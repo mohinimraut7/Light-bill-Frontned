dc3e2d1919b4f6fa9d915eae99402d9e8394fedc
mohinimraut7 <mohinimraut7@gmail.com>
2025-01-17 16:10:11 +0530

    change baseUrl

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 // export const baseUrl='https://lightbillsmanagement.onrender.com/api';
-// export const baseUrl='http://localhost:2000/api';
+export const baseUrl='http://localhost:2000/api';
 
-export const baseUrl='https://lightbillbackend.onrender.com/api'
+// export const baseUrl='https://lightbillbackend.onrender.com/api'
 
 
