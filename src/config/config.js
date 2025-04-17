e6a0737fd822a1ed54372b50c40c0dc241452536
mohinimraut7 <mohinimraut7@gmail.com>
2025-04-17 19:32:30 +0530

    url change

--- src/config/config.js
+++ src/config/config.js
@@ -6,10 +6,10 @@
 
  //export const baseUrl='http://localhost:2000/api';
 
-export const baseUrl='http://localhost:5000/api';
+// export const baseUrl='http://localhost:5000/api';
 
 // export const baseUrl='http://62.72.58.139:5000/api';
 
-// export const baseUrl='https://lightbillbackend.saavi.co.in/api'
+export const baseUrl='https://lightbillbackend.saavi.co.in/api'
 
 export const billBaseUrl = 'http://localhost:5000';
