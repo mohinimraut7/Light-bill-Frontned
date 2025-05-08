65af9ae630bca9915eaf7b5d6cea8e287e12b4b6
mohinimraut7 <mohinimraut7@gmail.com>
2025-05-08 17:51:42 +0530

    url changed

--- src/config/config.js
+++ src/config/config.js
@@ -1,13 +1,13 @@
 //export const baseUrl='https://lightbillsmanagement.onrender.com/api';
 
-export const baseUrl='https://lightbillbackend.onrender.com/api';
+// export const baseUrl='https://lightbillbackend.onrender.com/api';
 
 // export const baseUrl='https://lightbillapi.codifyinstitute.org/api';
 
  //export const baseUrl='http://localhost:2000/api';
 // export const baseUrl='http://62.72.58.139:5000/api';
 
-// export const baseUrl='http://localhost:5000/api';
+export const baseUrl='http://localhost:5000/api';
 export const billBaseUrl = 'http://localhost:5000';
 
 // export const baseUrl='https://lightbillbackend.saavi.co.in/api'
